601abeabb1fa64002806dd39d61bf31703849bfd
nouhatanan55-cloud <nouhatanan55@gmail.com>
2026-02-12 10:42:32 +0100

    Add files via upload

---
+++ projet.js
@@ -0,0 +1,13 @@
+function sir() {
+    window.location.href = "file:///C:/Users/USER/Desktop/projet%201/f2.html";
+}
+function rja3() {
+    window.location.href = "file:///C:/Users/USER/Desktop/projet%201/projet.html";
+    alert("The booking process was successful 🎊, Please be on time , we will contact you via Gmail.thanks");
+}
+              
+
+
+
+
+
